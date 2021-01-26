5c1621e063acefc978fac42aac032cddd1ad4866
Allyson Jeronimo <allysonjeronimo@gmail.com>
2021-01-26 11:20:17 -0300

    Setup toolbar and implementing MovieListFragment

---
+++ app/src/main/java/com/allysonjeronimo/muvis/ui/movielist/MovieListFragment.kt
@@ -0,0 +1,32 @@
+package com.allysonjeronimo.muvis.ui.movielist
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.allysonjeronimo.muvis.R
+
+class MovieListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MovieListFragment()
+    }
+
+    private lateinit var viewModel: MovieListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.movie_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MovieListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/allysonjeronimo/muvis/ui/movielist/MovieListViewModel.kt
@@ -0,0 +1,7 @@
+package com.allysonjeronimo.muvis.ui.movielist
+
+import androidx.lifecycle.ViewModel
+
+class MovieListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
