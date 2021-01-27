8f45ecd2627b638fc32f673179764a2dc945acf6
Allyson Jeronimo <allysonjeronimo@gmail.com>
2021-01-26 22:16:14 -0300

    Setup retrofit

--- app/src/main/java/com/allysonjeronimo/muvis/model/network/MovieDBApi.kt
+++ app/src/main/java/com/allysonjeronimo/muvis/model/network/MovieDBApi.kt
@@ -1,4 +1,12 @@
 package com.allysonjeronimo.muvis.model.network
 
+import com.allysonjeronimo.muvis.model.network.entity.MovieDBResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieDBApi {
+
+    @GET("/3/movie/popular")
+    fun movies(@Query("api_key") apiKey:String) : Call<MovieDBResponse>
 }
--- app/src/main/java/com/allysonjeronimo/muvis/ui/movielist/MovieListFragment.kt
+++ app/src/main/java/com/allysonjeronimo/muvis/ui/movielist/MovieListFragment.kt
@@ -1,32 +1,54 @@
 package com.allysonjeronimo.muvis.ui.movielist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.allysonjeronimo.muvis.R
+import com.allysonjeronimo.muvis.model.network.API_KEY
+import com.allysonjeronimo.muvis.model.network.BASE_URL
+import com.allysonjeronimo.muvis.model.network.MovieDBApi
+import com.allysonjeronimo.muvis.model.network.entity.MovieDBResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MovieListFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MovieListFragment()
-    }
+class MovieListFragment : Fragment(R.layout.movie_list_fragment) {
 
     private lateinit var viewModel: MovieListViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.movie_list_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MovieListViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val api = Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieDBApi::class.java)
+
+
+        api.movies(API_KEY).enqueue(object:Callback<MovieDBResponse>{
+            override fun onResponse(
+                call: Call<MovieDBResponse>,
+                response: Response<MovieDBResponse>
+            ) {
+                if(response.isSuccessful){
+
+                    Log.i(
+                        MovieListFragment::class.simpleName,
+                        response.body().toString()
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<MovieDBResponse>, t: Throwable) {
+                Log.e(MovieListFragment::class.simpleName, t.message.toString())
+            }
+        })
     }
 
 }
