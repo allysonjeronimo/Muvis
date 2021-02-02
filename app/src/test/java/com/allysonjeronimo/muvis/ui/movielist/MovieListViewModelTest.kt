b4749d7893db29b3ead4747abe25e755abecff1a
Allyson Jeronimo <allysonjeronimo@gmail.com>
2021-02-02 10:17:08 -0300

    MovieListViewModel test implemented

--- app/src/test/java/com/allysonjeronimo/muvis/ui/movielist/MovieListViewModelTest.kt
+++ app/src/test/java/com/allysonjeronimo/muvis/ui/movielist/MovieListViewModelTest.kt
@@ -2,6 +2,7 @@ package com.allysonjeronimo.muvis.ui.movielist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.allysonjeronimo.muvis.R
 import com.allysonjeronimo.muvis.model.db.entity.Movie
 import com.allysonjeronimo.muvis.repository.MovieRepository
 import io.mockk.coEvery
@@ -22,10 +23,12 @@ class MovieListViewModelTest{
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
+
     private val testDispatcher = TestCoroutineDispatcher()
 
     private val repository = mockk<MovieRepository>()
     private val moviesLiveDataObserver = mockk<Observer<List<Movie>>>(relaxed = true)
+    private val errorOnLoadingLiveDataObserver  = mockk<Observer<Int>>(relaxed = true)
 
     @Before
     fun setup(){
@@ -39,7 +42,7 @@ class MovieListViewModelTest{
     }
 
     @Test
-    fun `when view model load movies then it should call the repository`(){
+    fun `when view model loadMovies gets success then sets moviesLiveData`(){
 
         val viewModel = instantiateViewModel()
 
@@ -63,9 +66,29 @@ class MovieListViewModelTest{
         }
     }
 
+    @Test
+    fun `when view model loadMovies gets exception then sets errorOnLoadingLiveData`(){
+
+        val viewModel = instantiateViewModel()
+
+        coEvery {
+            repository.getMovies()
+        } throws Exception()
+
+        viewModel.loadMovies()
+
+        coVerify {
+            repository.getMovies()
+        }
+        coVerify {
+            errorOnLoadingLiveDataObserver.onChanged(R.string.movie_list_error_on_loading)
+        }
+    }
+
     private fun instantiateViewModel() : MovieListViewModel {
         val viewModel = MovieListViewModel(repository)
         viewModel.moviesLiveData.observeForever(moviesLiveDataObserver)
+        viewModel.errorOnLoadingLiveData.observeForever(errorOnLoadingLiveDataObserver)
         return viewModel
     }
 }
