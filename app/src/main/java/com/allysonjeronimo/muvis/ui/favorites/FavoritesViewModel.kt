6d56a36a7ede0d50bd09cf3d66a63f738e97ea7c
Allyson Jeronimo <allysonjeronimo@gmail.com>
2021-02-08 10:25:45 -0300

    BottomNavigation with NavController implemented

---
+++ app/src/main/java/com/allysonjeronimo/muvis/ui/favorites/FavoritesViewModel.kt
@@ -0,0 +1,7 @@
+package com.allysonjeronimo.muvis.ui.favorites
+
+import androidx.lifecycle.ViewModel
+
+class FavoritesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/allysonjeronimo/muvis/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.allysonjeronimo.muvis.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
